125a6abceda774e565c2a6d9afef8f36d3b745f7
wardbox <empirebillboard@gmail.com>
2022-04-26 23:53:23 -0700

    filter topic call as well

--- app.py
+++ app.py
@@ -16,6 +16,13 @@ limiter = Limiter(
     default_limits=["10 per day", "5 per hour", "3 per minute"]
 )
 
+button_texts = [
+    "that sounds boring (－_－) zzZ",
+    "nope (ﾒ` ﾛ ´)",
+    "ew gross (눈_눈)",
+    "try again 	(╯︵╰,)"
+]
+
 
 @app.route("/")
 def index():
@@ -24,6 +31,54 @@ def index():
         prompt="Give me a topic that many people will be familiar with.",
         temperature=0.9
     )
+
+    filtered_topic = openai.Completion.create(
+        engine="content-filter-alpha",
+        prompt="<|endoftext|>"+topic.choices[0].text+"\n--\nLabel:",
+        temperature=0,
+        max_tokens=1,
+        top_p=0,
+        logprobs=10
+    )
+
+    output_label = filtered_topic["choices"][0]["text"]
+
+    # This is the probability at which we evaluate that a "2" is likely real
+    # vs. should be discarded as a false positive
+    toxic_threshold = -0.355
+
+    if output_label == "2":
+        # If the model returns "2", return its confidence in 2 or other output-labels
+        logprobs = filtered_topic["choices"][0]["logprobs"]["top_logprobs"][0]
+
+        # If the model is not sufficiently confident in "2",
+        # choose the most probable of "0" or "1"
+        # Guaranteed to have a confidence for 2 since this was the selected token.
+        if logprobs["2"] < toxic_threshold:
+            logprob_0 = logprobs.get("0", None)
+            logprob_1 = logprobs.get("1", None)
+
+            # If both "0" and "1" have probabilities, set the output label
+            # to whichever is most probable
+            if logprob_0 is not None and logprob_1 is not None:
+                if logprob_0 >= logprob_1:
+                    output_label = "0"
+                else:
+                    output_label = "1"
+            # If only one of them is found, set output label to that one
+            elif logprob_0 is not None:
+                output_label = "0"
+            elif logprob_1 is not None:
+                output_label = "1"
+
+            # If neither "0" or "1" are available, stick with "2"
+            # by leaving output_label unchanged.
+
+    # if the most probable token is none of "0", "1", or "2"
+    # this should be set as unsafe
+    if output_label not in ["0", "1", "2"]:
+        output_label = "2"
+        return redirect(url_for('index.html'), try_again=True)
 
     response = openai.Completion.create(
         engine="text-davinci-002",
@@ -35,14 +90,7 @@ def index():
         max_tokens=250
     )
 
-    button_texts = [
-        "that sounds boring (－_－) zzZ",
-        "nope (ﾒ` ﾛ ´)",
-        "ew gross (눈_눈)",
-        "try again 	(╯︵╰,)"
-    ]
-
-    filtered = openai.Completion.create(
+    filtered_response = openai.Completion.create(
         engine="content-filter-alpha",
         prompt="<|endoftext|>"+response.choices[0].text+"\n--\nLabel:",
         temperature=0,
@@ -51,7 +99,7 @@ def index():
         logprobs=10
     )
 
-    output_label = filtered["choices"][0]["text"]
+    output_label = filtered_response["choices"][0]["text"]
 
     # This is the probability at which we evaluate that a "2" is likely real
     # vs. should be discarded as a false positive
@@ -59,7 +107,7 @@ def index():
 
     if output_label == "2":
         # If the model returns "2", return its confidence in 2 or other output-labels
-        logprobs = response["choices"][0]["logprobs"]["top_logprobs"][0]
+        logprobs = filtered_response["choices"][0]["logprobs"]["top_logprobs"][0]
 
         # If the model is not sufficiently confident in "2",
         # choose the most probable of "0" or "1"
@@ -112,12 +160,12 @@ def index():
 @app.errorhandler(429)
 def ratelimit_handler(e):
     return render_template(
-        '429.html', error=e
+        '429.html', button_text="slappa"
     )
 
 
 @app.errorhandler(404)
 def ratelimit_handler(e):
     return render_template(
-        '404.html', error=e
+        '404.html', button_text="not all those who wander are lost"
     )
